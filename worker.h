c380b0d1fec5d39e7bdce346f48d7e51599fafe5
Poornima Nookala <nookala.poornima@gmail.com>
2017-08-08 13:31:33 -0500

    changed test input

--- worker.h
+++ worker.h
@@ -19,9 +19,9 @@
 //
 //static int WORKERS;
 //
-//#define NUM_SAMPLES 16//262144//8388608 //2^23
+//#define NUM_SAMPLES 262144//8388608 //2^23
 //#define QUEUE_SIZE NUM_SAMPLES   // Define maximum length of the queue
-//#define NUM_CPUS 2
+//#define NUM_CPUS 24
 //
 //#define INITQUEUES(size) InitBasicQueue(size)
 //#define ENQUEUE(data) BasicEnqueue(data)
